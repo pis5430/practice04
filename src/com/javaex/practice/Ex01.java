3906cada3c51c81b2acec091e08b4b7086b7c541
pyo i seul <isl1089@naver.com>
2020-11-19 16:36:19 +0900

    11/19 practice04 2st commit

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -3,8 +3,31 @@ package com.javaex.practice;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] intArray = new int[5];
+		
+		intArray[0]=1;
+		intArray[1]=2;
+		intArray[2]=3;
+		intArray[3]=4;
+		intArray[4]=5;
+		
+		int result = 0;
+		
+		for(int i =0; i<intArray.length; i++) {
+			
+			//result =result + intArray[i];
+			result += intArray[i]; //
+			
+			// result = 0 초기값선언
+			// result = 0 + 1
+			// result = 1 + 2
+			// result = 3 + 3
+			// result = 6 + 4
+			// result = 10 + 5 //결과값 15
+		}
+		
+		System.out.println(result);
+		
 	}
 
 }
---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,40 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		
+		double[] num = new double[] {1.2,3.3,6.7};
+		
+		int i; //횟수
+		
+		
+		// 순서대로 나오는 방법
+		
+		/*
+		for(i=0; i<num.length; i++) {
+			
+			System.out.println(num[i]);
+		}
+		*/
+		
+		// 반대방향으로 나오는 방법01
+		
+		/*
+		for(i=2; i<=num.length-1 && i>=0; i--) {
+		
+			System.out.println(num[i]);
+		}
+		*/
+		
+		// 반대방향으로 나오는 방법02
+		
+		for(i=num.length-1; i>=0; i--) {
+			
+			System.out.println(num[i]);
+		}
+
+	}
+
+}

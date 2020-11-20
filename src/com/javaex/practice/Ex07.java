58a9040c9ca796e9b206d4afecaef6d27247750c
pyo i seul <isl1089@naver.com>
2020-11-20 09:54:16 +0900

    11/20 practice04 1st commit

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -8,20 +8,22 @@ public class Ex07 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int[] wonArray = new int[10];
-		wonArray[0] = 50000;
-		wonArray[1] = 10000;
-		wonArray[2] = 5000;
-		wonArray[3] = 1000;
-		wonArray[4] = 500;
-		wonArray[5] = 100;
-		wonArray[6] = 50;
-		wonArray[7] = 10;
-		wonArray[8] = 5;
-		wonArray[9] = 1;
+		int[] wonArray = new int[] {50000,10000,5000,1000,500,100,50,10,5,1};
 		
+		int count=0;
+		
+		//배열 개수 10개
+
+		System.out.print("금액: ");
 		int money = sc.nextInt();
 		
+		for(int i =0; i<wonArray.length; i++) {
+			
+			count = money/wonArray[i]; // 개수
+			money -= (wonArray[i]*count); // 앞에서 계산된 갚을 빼주기
+		
+			System.out.println(wonArray[i] +"원: " + count+"개");
+		}
 		
 		
 		
